b89b3f8231f168fe76c05ae05d74369d37c5b566
johnjovan <emorock0925@gmail.com>
2020-03-12 21:26:25 +0800

    ITTC Student

---
+++ js/ITTC-Student.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+  $("#search").click(function () {
+    $(".menu-item").addClass("hide-item");
+    $(".search-form").addClass("active");
+    $(".close").addClass("active");
+    $("#search").hide();
+    $("#sign-in").hide();
+  });
+  $(".close").click(function () {
+    $(".menu-item").removeClass("hide-item");
+    $(".search-form").removeClass("active");
+    $(".close").removeClass("active");
+    $("#search").show();
+    $("#sign-in").show();
+  });
+});
+
+$(document).ready(function () {
+  $(".menu-toggle").click(function () {
+    $(".menu-toggle").toggleClass("active");
+    $("nav").toggleClass("active");
+    //! NOT FINISH //
+    $(".icon").hide();
+  }); //! NOT FINISH //
+  $("#bar").click(function () {
+    $(".icon").show();
+  });
+});
+
+
+function resettext() {
+  document.getElementById("myform").reset();
+}
