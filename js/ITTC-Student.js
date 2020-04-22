d29aaf322aab6b91f236759f8d301857755671d7
johnjovan <emorock0925@gmail.com>
2020-04-22 18:02:35 +0800

    ITTC Student

--- js/ITTC-Student.js
+++ js/ITTC-Student.js
@@ -53,10 +53,10 @@ var swiper = new Swiper('.swiper-container', {
   // centeredSlides: true,
   loop: true,
   loopFillGroupWithBlank: true,
-  // autoplay: {
-  //   delay: 2500,
-  //   disableOnInteraction: false,
-  // },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
