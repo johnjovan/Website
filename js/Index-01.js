95ae6d5a9ca9bd1cdf25b7e6debf466172d05c10
johnjovan <emorock0925@gmail.com>
2020-05-15 17:01:38 +0800

    Index 01

--- js/Index-01.js
+++ js/Index-01.js
@@ -1,3 +1,8 @@
 setTimeout(function () {
-    $('.loader-bg').fadeToggle(1000);
+    $('.loader-bg').fadeToggle(100);
 }, 2000);
+
+window.addEventListener("scroll", function () {
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 100);
+});
