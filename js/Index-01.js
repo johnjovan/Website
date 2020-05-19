45045937b7991590bb612c611f2a1d3fbc41366e
johnjovan <emorock0925@gmail.com>
2020-05-19 15:41:31 +0800

    Index 01

--- js/Index-01.js
+++ js/Index-01.js
@@ -1,8 +1,42 @@
+//!  Sidebar  !// 
 setTimeout(function () {
     $('.loader-bg').fadeToggle(100);
 }, 2000);
 
+//!  Header Sticky  !// 
 window.addEventListener("scroll", function () {
     var header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 100);
+});
+
+// $(document).ready(function () {
+//     $("#sidebar-open").click(function () {
+//         $("#sidebar").slideToggle();
+//     });
+//     $("#sidebar-close").click(function () {
+//         $("#sidebar").slideToggle();
+//     });
+// });
+
+//!  Sidebar  !// 
+var t1 = new TimelineMax({
+    paused: true
+});
+
+t1.to("#sidebar", 0.4, {
+    right: "0%",
+});
+
+t1.staggerFrom(".sidebar-link", 0.4, {
+    opacity: 0
+}, 0.1, "-=0.2");
+
+t1.reverse();
+
+$(document).on("click", "#sidebar-open", function () {
+    t1.reversed(!t1.reversed());
+});
+
+$(document).on("click", "#sidebar-close", function () {
+    t1.reversed(!t1.reversed());
 });
